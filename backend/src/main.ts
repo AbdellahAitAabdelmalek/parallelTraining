d7099a93cb66eead92d1fcc1282a22540f3594ed
AIT ABDELMALEK Abdellah <abdellahaa@bam.tech>
2026-03-15 09:32:00 +0100

    feat: update server startup to use PORT environment variable for listening

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,9 @@ for (const key of REQUIRED_ENV_VARS) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({});
-  await app.listen(3000);
-  console.log("Backend running on http://localhost:3000");
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Backend running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
